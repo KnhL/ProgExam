49f37ee8e5c09f51c78f04163f391b3530ac12ae
F0gboy <Jasp3r@live.dk>
2023-03-14 13:58:25 +0100

    stads

--- Assets/Scripts/RoomGeneration.cs
+++ Assets/Scripts/RoomGeneration.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class RoomGeneration : MonoBehaviour
 {
-    Collider m_Collider;
-    Vector3 m_Center;
-    Vector3 m_Size, m_Min, m_Max;
+    private RaycastHit[] hits;
+    public List<GameObject> wallList;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Collider = GetComponent<Collider>();
-        
-    }
-
-    public void GetColliderBoundaries()
-    {
-        var bounds = m_Collider.bounds;
-        m_Center = bounds.center;
-        m_Size = bounds.size;
-        m_Min = bounds.min;
-        m_Max = bounds.max;
-    }
-
-    public void Kitchen()
-    {
-        
-        
-    }
-
-    public void LivingRoom()
-    {
-        
-        
-    }
-
-    public void Bedroom()
-    {
-        
-        
+        wallList = new List<GameObject>();
+        findObjects();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void findObjects()
+    {
+        Vector3[] directions = { transform.forward, transform.right, -transform.right, -transform.forward, -transform.up };
+        hits = new RaycastHit[5];
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Ray ray = new Ray(transform.position, directions[i]);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                hits[i] = hit;
+                if (!wallList.Contains(hit.collider.gameObject))
+                {
+                    wallList.Add(hit.collider.gameObject);
+                }
+            }
+            else
+            {
+                hits[i] = new RaycastHit();
+            }
+        }
+
+        Debug.Log("Wall list:");
+        foreach (GameObject wall in wallList)
+        {
+            Debug.Log(wall.name);
+        }
     }
 }
